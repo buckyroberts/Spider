484f36e0da81c17f573e47dcd2887fd7505ec93d
Bucky <buckyroberts@gmail.com>
2016-02-24 18:35:52 -0500

    Cleaning up code.

--- main.py
+++ main.py
@@ -4,11 +4,10 @@ from link_finder import LinkFinder
 
 def get_page_links(base_url, page_url):
     response = urlopen(base_url)
+    html_string = ''
     if response.getheader('Content-Type') == 'text/html':
         html_response = response.read()
         html_string = html_response.decode("utf-8")
-    else:
-        html_string = ''
     link_finder = LinkFinder(base_url, page_url)
     link_finder.feed(html_string)
     return link_finder.get_page_links()
