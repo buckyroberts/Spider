7206b732a9403c5e14240fd94c10bf7080a36e7e
Bucky <buckyroberts@gmail.com>
2016-02-26 20:15:15 -0500

    Cleaning up code.

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ from spider import Spider
 from domain import *
 from general import *
 
-PROJECT_NAME = 'thenewboston'
-HOMEPAGE = 'https://thenewboston.com/'
+PROJECT_NAME = 'example'
+HOMEPAGE = 'http://example.com/'
 DOMAIN_NAME = get_domain_name(HOMEPAGE)
 
 QUEUE_FILE = PROJECT_NAME + '/queue.txt'
