22debeb86d0e74713b5aed47918d57d8c19bdaab
tedmx <tedmx@ya.ru>
2016-02-28 23:04:44 +0300

    Add argument passing logic
    Implement "flush" command which erases contents of data files before new
    crawl, fix several flake8 issues, change interface of Spider initializer
    to accept dictionary instead of four arguments.

--- spider.py
+++ spider.py
@@ -14,12 +14,14 @@ class Spider:
     queue = set()
     crawled = set()
 
-    def __init__(self, project_name, base_url, domain_name):
-        Spider.project_name = project_name
-        Spider.base_url = base_url
-        Spider.domain_name = domain_name
+    def __init__(self, attrs):
+        Spider.project_name = attrs['project_name']
+        Spider.base_url = attrs['base_url']
+        Spider.domain_name = attrs['domain_name']
         Spider.queue_file = Spider.project_name + '/queue.txt'
         Spider.crawled_file = Spider.project_name + '/crawled.txt'
+        if(attrs["flush"]):
+            remove_data_files(Spider.project_name, Spider.base_url)
         self.boot()
         self.crawl_page('Initializer', Spider.base_url)
 
