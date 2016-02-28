397e4bc49497ff83d1e9a11ec0c3a931ffc10c69
OscarEReyes <OscarEReyes@users.noreply.github.com>
2016-02-28 12:50:32 -0600

    Update spider.py
    Simplified some class variable assignments and changed some print statements.

--- spider.py
+++ spider.py
@@ -6,11 +6,7 @@ from general import *
 class Spider:
 
     # Class variables (shared among all instances)
-    project_name = ''
-    base_url = ''
-    domain_name = ''
-    queue_file = ''
-    crawled_file = ''
+    project_name =  base_url = domain_name = queue_file =  crawled_file = ''
     queue = set()
     crawled = set()
 
@@ -33,8 +29,10 @@ class Spider:
     @staticmethod
     def crawl_page(thread_name, page_url):
         if page_url not in Spider.crawled:
-            print(thread_name + ' crawling: ' + page_url)
-            print('Queue ' + str(len(Spider.queue)) + ' | Crawled ' + str(len(Spider.crawled)))
+            current_queue = len(Spider.queue)
+            number_crawled = len(Spider.crawled)
+            print('{} crawling: {}'.format(thread_name, page_url))
+            print('Queue {} | Crawled {}'.format(current_queue, number_crawled ))
             Spider.add_links_to_queue(Spider.gather_links(page_url))
             Spider.queue.remove(page_url)
             Spider.crawled.add(page_url)
@@ -63,9 +61,7 @@ class Spider:
     @staticmethod
     def add_links_to_queue(links):
         for url in links:
-            if url in Spider.queue:
-                continue
-            if url in Spider.crawled:
+            if url in Spider.queue or Spider.crawled:
                 continue
             if Spider.domain_name not in url:
                 continue
