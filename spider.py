97613a28835f778f37fd65b520c26b35ffdf88d8
Braxton Fair <braxton@hexel.xyz>
2016-03-03 16:07:21 -0600

    Fix typo

--- spider.py
+++ spider.py
@@ -44,7 +44,7 @@ class Spider:
         html_string = ''
         try:
             response = urlopen(page_url)
-            if 'text/html' in respinse.getheader():
+            if 'text/html' in response.getheader():
                 html_bytes = response.read()
                 html_string = html_bytes.decode("utf-8")
             finder = LinkFinder(Spider.base_url, page_url)
