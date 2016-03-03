f8cfe0a4e0c9ced13cbc34a0f6315a9283fd5a61
nicowaltz <nico@waltz.de>
2016-03-03 18:45:22 +0100

    documentation fix

--- main.py
+++ main.py
@@ -3,7 +3,7 @@
 
     Examples:
         python3 main.py -p thenewboston -u https://thenewboston.com         # Specified project folder
-        python3 main.py -u https://thenewboston.com                         # Creates project folder 
+        python3 main.py -u https://thenewboston.com                         # Creates project folder thenewboston.com
         python3 main.py -p thenewboston -u https://thenewboston.com -j20    # 20 threads
         python3 main.py -h                                                  # Displays usage
 
