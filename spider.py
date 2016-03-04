059692a963c97ce877688d7b4155f918c17e352b
Bucky <buckyroberts@gmail.com>
2016-03-04 11:16:10 -0500

    Fixing getheader('Content-Type') value to simplify code.

--- spider.py
+++ spider.py
@@ -44,14 +44,13 @@ class Spider:
         html_string = ''
         try:
             response = urlopen(page_url)
-            if response.getheader('Content-Type') == 'text/html' or response.getheader(
-                    'content-type') == 'text/html;charset=utf-8':
+            if 'text/html' in response.getheader('Content-Type'):
                 html_bytes = response.read()
                 html_string = html_bytes.decode("utf-8")
             finder = LinkFinder(Spider.base_url, page_url)
             finder.feed(html_string)
-        except:
-            print('Error: can not crawl page')
+        except Exception as e:
+            print(str(e))
             return set()
         return finder.page_links()
 
