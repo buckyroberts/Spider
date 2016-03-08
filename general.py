71325d7ed70cf38aaf192050ae0d9c697ab90d12
srinath29 <srinath.prathi@gmail.com>
2016-03-08 18:00:50 +0530

    used join function in the os.path to make it more pythonic

--- general.py
+++ general.py
@@ -10,8 +10,8 @@ def create_project_dir(directory):
 
 # Create queue and crawled files (if not created)
 def create_data_files(project_name, base_url):
-    queue = project_name + '/queue.txt'
-    crawled = project_name + '/crawled.txt'
+    queue = os.path.join(project_name , 'queue.txt')
+    crawled = os.path.join(project_name,"crawled.txt")
     if not os.path.isfile(queue):
         write_file(queue, base_url)
     if not os.path.isfile(crawled):
