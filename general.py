d13e83c793b6bedee2417de225b9adc799ee962e
Bucky <buckyroberts@gmail.com>
2016-05-04 17:27:14 -0400

    Fixed file path bug

--- general.py
+++ general.py
@@ -45,7 +45,7 @@ def file_to_set(file_name):
 
 
 # Iterate through a set, each item will be a line in a file
-def set_to_file(links, file):
-    with open(filename,"w") as f:
+def set_to_file(links, file_name):
+    with open(file_name,"w") as f:
         for l in sorted(links):
             f.write(l+"\n")
