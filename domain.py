5f1e091ef64a6c0f03e87b0f2182a91e8cf9678c
Charles Lien <lienching31240@gmail.com>
2016-08-08 22:51:19 +0800

    make get_domain_name able to handle longer domain name

--- domain.py
+++ domain.py
@@ -1,11 +1,17 @@
 from urllib.parse import urlparse
 
+top_level_domain= set(["com","org","net","int","edu","gov","mil"])
 
 # Get domain name (example.com)
 def get_domain_name(url):
     try:
         results = get_sub_domain_name(url).split('.')
-        return results[-2] + '.' + results[-1]
+        domain_name = results[-2] + '.' + results[-1]
+        for x in range(len(results)-3,0,-1):
+            domain_name = results[x] + '.' + domain_name
+            if results[len(results)-x] not in top_level_domain:
+                break
+        return domain_name
     except:
         return ''
 
@@ -16,3 +22,4 @@ def get_sub_domain_name(url):
         return urlparse(url).netloc
     except:
         return ''
+
