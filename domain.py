4c0e9cd8c49ca9daee0003bf0b4972d6545d8004
Charles Lien <lienching31240@gmail.com>
2016-08-19 00:18:49 +0800

    fix top-level-domain issue by using tld module

--- domain.py
+++ domain.py
@@ -1,19 +1,12 @@
 from urllib.parse import urlparse
+from tld import get_tld
 
-top_level_domain= set(["com","org","net","int","edu","gov","mil"])
 
 # Get domain name (example.com)
 def get_domain_name(url):
-    try:
-        results = get_sub_domain_name(url).split('.')
-        domain_name = results[-2] + '.' + results[-1]
-        for x in range(len(results)-3,0,-1):
-            domain_name = results[x] + '.' + domain_name
-            if results[len(results)-x] not in top_level_domain:
-                break
-        return domain_name
-    except:
-        return ''
+	# Use tld to resovle the top-level-domain
+
+    return get_tld(url)
 
 
 # Get sub domain name (name.example.com)
@@ -22,4 +15,5 @@ def get_sub_domain_name(url):
         return urlparse(url).netloc
     except:
         return ''
+
 
