e0e4e24c0d2a7d4c3b22662680def160895fd1f8
Cyber FOrce <CyberWarriors@users.noreply.github.com>
2016-08-20 19:05:13 -0600

    LinkFinder.py
    Added comments and example and changed somethings

--- link_finder.py
+++ link_finder.py
@@ -4,6 +4,7 @@ from urllib import parse
 
 class LinkFinder(HTMLParser):
 
+    # setting up our program
     def __init__(self, base_url, page_url):
         super().__init__()
         self.base_url = base_url
@@ -12,14 +13,22 @@ class LinkFinder(HTMLParser):
 
     # When we call HTMLParser feed() this function is called when it encounters an opening tag <a>
     def handle_starttag(self, tag, attrs):
+        
+        # we are checking the tag name and we are using the tag if it is a ancher tag like this <a href="example.com" />
         if tag == 'a':
             for (attribute, value) in attrs:
                 if attribute == 'href':
+                    
+                    # Example /user/form/submit is gonna become https://www.google.com/user/form/submit
                     url = parse.urljoin(self.base_url, value)
+                    
+                    # we are adding to the set() of links 
                     self.links.add(url)
-
+    
+    # all this module does is return the set of links to the main module
     def page_links(self):
         return self.links
-
+    
+    # this is like a try and catch for parsing our html using the HTMLParser module
     def error(self, message):
         pass
